d50606de8e2a88c99de99e11580c87fd9515d52d
ravenjolly <ravenjolly07@gmail.com>
2023-03-07 21:59:34 +0000

    Working copy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,8 +2,8 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
 
-    app.use(proxy('/api', { target: 'http://localhost:8080/', changeOrigin: true }));
-   app.use(proxy('/account', { target: 'http://localhost:8081/' , changeOrigin: true})); 
+    app.use(proxy('/api', { target: 'http://localhost/'}));
+   app.use(proxy('/account', { target: 'http://localhost'})); 
 
 	//app.use(proxy('/api', { target: 'http://'+ window._env_.REACT_APP_API_IP +'/' }));
 	//app.use(proxy('/account', { target: 'http://' + window._env_.REACT_APP_AUTH_IP + '/' }));
